393d50278fced7708de0686973473885a654f694
Harmones81 <52055382+Harmones81@users.noreply.github.com>
2023-04-12 17:13:57 -0400

    Added database classes

---
+++ src/model/SQLiteDB.java
@@ -0,0 +1,48 @@
+package model;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class SQLiteDB extends DB
+{
+
+    /** Constructor: SQLiteDB
+     * @throws SQLException
+     * @throws ClassNotFoundException
+     */
+    public SQLiteDB() throws SQLException, ClassNotFoundException {
+        //Build the connection String
+        sJdbc = "jdbc:sqlite";
+        sDriverName = "org.sqlite.JDBC";
+        // register the driver
+        Class.forName(sDriverName);
+        sDbUrl = sJdbc + ":" + dbName;
+        conn = DriverManager.getConnection(sDbUrl);
+    }
+
+    /** Constructor: SQLiteDB
+     * @param dbName
+     * @throws SQLException
+     * @throws ClassNotFoundException
+     */
+    public SQLiteDB(String dbName) throws SQLException, ClassNotFoundException {
+        //Build the connection String
+        sJdbc = "jdbc:sqlite";
+        sDriverName = "org.sqlite.JDBC";
+        // register the driver
+        Class.forName(sDriverName);
+        sDbUrl = sJdbc + ":" + dbName;
+
+        this.dbName = dbName;
+        conn = DriverManager.getConnection(sDbUrl);
+    }
+
+    /** Method: close
+     * Purpose: Close the database connection
+     * @throws SQLException
+     * @return void
+     */
+    public void close() throws SQLException {
+        conn.close();
+    }
+}
