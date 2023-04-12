2a3c38f70b5cf2ad0443559cc7accecf0086cbb4
Gild <109309201+stephenvilleda@users.noreply.github.com>
2023-04-12 17:07:02 -0400

    Classes initialized

--- src/controller/Player.java
+++ src/controller/Player.java
@@ -1,4 +1,22 @@
 package controller;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 public class Player {
+    private String name;
+    private int curRoom;
+    private Collection<ArrayList<Item>> inventory;
+    public int getCurRoom() {
+        return this.curRoom;
+    }
+    public void setCurRoom(int curRoom) {
+        this.curRoom = curRoom;
+    }
+    public Collection<ArrayList<Item>> getInventory() {
+        return this.inventory;
+    }
+    public void setInventory(Collection<ArrayList<Item>> inventory) {
+        this.inventory = inventory;
+    }
 }
