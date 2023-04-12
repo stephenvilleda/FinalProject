d9069cd253ea9c7b0715a6e9bb560f784b1e2df6
Gild <109309201+stephenvilleda@users.noreply.github.com>
2023-04-12 17:09:38 -0400

    Classes initialized

--- src/controller/Item.java
+++ src/controller/Item.java
@@ -1,4 +1,31 @@
 package controller;
 
 public class Item {
+    private int itemID;
+    private String itemName;
+    private String itemDescription;
+
+    public int getItemID() {
+        return this.itemID;
+    }
+
+    public void setItemID(int itemID) {
+        this.itemID = itemID;
+    }
+
+    public String getItemName() {
+        return this.itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public String getItemDescription() {
+        return this.itemDescription;
+    }
+
+    public void setItemDescription(String itemDescription) {
+        this.itemDescription = itemDescription;
+    }
 }
