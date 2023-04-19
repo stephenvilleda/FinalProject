e3790eabe22df59dbb368988a2aa25cef207aadc
raEliminator22 <123002072+raEliminator22@users.noreply.github.com>
2023-04-19 12:21:13 -0400

    commands was slightly modified

--- src/controller/Commands.java
+++ src/controller/Commands.java
@@ -1,4 +1,19 @@
 package controller;
 
 public class Commands {
+
+    private Player player;
+
+    Commands() {
+        this.player = new Player();
+    }
+
+    protected  String excuteCommand(String cmd) {
+        return cmd;
+    }
+
+    private String move(String cmdRoom) {
+
+        return cmdRoom;
+    }
 }
