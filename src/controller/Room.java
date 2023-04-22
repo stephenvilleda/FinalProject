bee5b82349985cd2d8a2a7ee6a89f1865a335f96
raEliminator22 <123002072+raEliminator22@users.noreply.github.com>
2023-04-22 02:05:22 -0400

    room class

--- src/controller/Room.java
+++ src/controller/Room.java
@@ -1,9 +1,26 @@
 package controller;
 
-public class Room {
+import gameExceptions.GameException;
+
+public class Room<String> {
 
     private int roomId;
     private String roomName;
+
+    private String roomdescription;
+
+    private ArrayList<interger> items;
+
+    private boolean  roomvisited;
+
+
+    public Room() throws GameException {
+        roomId = 1;
+        roomName = "";
+        roomdescription = "";
+    }
+
+
 
 
     public Room(int id) {
@@ -25,4 +42,58 @@ public class Room {
     public String getRoomName() {
         return this.roomName;
     }
+
+    public void setRoomdescription(String roomdescription) {
+        this.roomdescription = roomdescription;
+    }
+
+    public String getDescription()
+    {
+        return this.description;
+    }
+
+    public ArrayList<Integer> getItems()
+    {
+        return this.items;
+    }
+
+    public void setItems(ArrayList<Integer> items)
+    {
+        this.items = items;
+    }
+
+    public boolean isRoomvisitedVisited()
+    {
+        return this.rvisited;
+    }
+
+    public void setRoomvisitedVisited(boolean roomvisited) {
+        this.roomvisited = roomvisited;
+    }
+
+
+
+    public <String> String display() throws GameException
+    {
+        return buildDescription() + buildItems() + displayExits();
+    }
+
+    private String buildDescription()
+    {
+        StringBuilder sb = new StringBuilder();
+        if (! isVisited())
+            if (roomID == 1)
+                sb.append("You find yourself in the ");
+            else
+                sb.append("You enter into into the ");
+        else
+            sb.append("You return to the ");
+
+        sb.append(getRoomName()).append(".\n");
+        sb.append(getDescription());
+        return sb.toString();
+    }
+     
+
+
 }
